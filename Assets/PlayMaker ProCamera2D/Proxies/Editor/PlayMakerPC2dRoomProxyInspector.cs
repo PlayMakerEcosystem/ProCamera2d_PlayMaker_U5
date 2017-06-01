4a991faa756ed1320e8e6f4be0dba03a3e762cbd
Jean Fabre <contact@fabrejean.net>
2017-06-01 09:19:24 +0300

    Finished PlayMakerPC2dRoomProxyInspector

---
+++ Assets/PlayMaker ProCamera2D/Proxies/Editor/PlayMakerPC2dRoomProxyInspector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(PlayMakerPC2dRoomProxy))]
+public class PlayMakerPC2dRoomProxyInspector : Editor
+{
+	PlayMakerPC2dRoomProxy _target;
+
+
+	public override void OnInspectorGUI()
+	{
+		_target = (PlayMakerPC2dRoomProxy)target;
+
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("debug"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("proCamera2DRooms"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("eventTarget"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("onStartedTransition"));
+		if (!_target.onStartedTransition.isNone)
+		{
+			EditorGUI.indentLevel++;
+			EditorGUILayout.LabelField("Event Properties","'roomIndex' 'previousRoomIndex'");
+			EditorGUI.indentLevel--;
+		}
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("onFinishedTransition"));
+		if (!_target.onFinishedTransition.isNone)
+		{
+			EditorGUI.indentLevel++;
+			EditorGUILayout.LabelField("Event Properties","'roomIndex' 'previousRoomIndex'");
+			EditorGUI.indentLevel--;
+		}
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("onExitedAllRooms"));
+
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+}
+
--- Assets/PlayMaker ProCamera2D/Proxies/PlayMakerPC2dRoomProxy.cs
+++ Assets/PlayMaker ProCamera2D/Proxies/PlayMakerPC2dRoomProxy.cs
@@ -10,21 +10,28 @@ using Com.LuisPedroFonseca.ProCamera2D;
 
 public class PlayMakerPC2dRoomProxy : MonoBehaviour {
 
+	[UnityEngine.Tooltip("Output logs for each event being fired")]
 	public bool debug = false;
 
-	[ExpectComponent(typeof(ProCamera2DRooms))]
-	public Owner proCamera2DRooms;
 
+	[ExpectComponent(typeof(ProCamera2DRooms))]
+	[UnityEngine.Tooltip("The GameObject with a Camera, a ProCamera2D component and the Rooms extension enabled")]
+	public MainCameraTarget proCamera2DRooms;
+
+	[UnityEngine.Tooltip("The PlayMaker events target")]
 	public PlayMakerEventTarget eventTarget = new PlayMakerEventTarget(true);
 
+	[UnityEngine.Tooltip("Event sent when a room transition started. \nUse GetEventProperty with key 'roomIndex' and 'previousRoomIndex' to retrieve the rooms index of that transition" )]
 	[EventTargetVariable("eventTarget")]
 	[ShowOptions]
 	public PlayMakerEvent onStartedTransition = new PlayMakerEvent();
 
+	[UnityEngine.Tooltip("Event sent when a room transition finished. \nUse GetEventProperty with key 'roomIndex' and 'previousRoomIndex' to retrieve the rooms index of that transition" )]
 	[EventTargetVariable("eventTarget")]
 	[ShowOptions]
 	public PlayMakerEvent onFinishedTransition = new PlayMakerEvent();
 
+	[UnityEngine.Tooltip("Event sent when all rooms where exited." )]
 	[EventTargetVariable("eventTarget")]
 	[ShowOptions]
 	public PlayMakerEvent onExitedAllRooms = new PlayMakerEvent();
@@ -57,6 +64,11 @@ public class PlayMakerPC2dRoomProxy : MonoBehaviour {
 
 	void OnDisable()
 	{
+		if (_pc2dRooms== null)
+		{
+			return;
+		}
+		
 		if (!onStartedTransition.isNone) _pc2dRooms.OnStartedTransition.RemoveListener (onStartedTransitionCallBack);
 		if (!onFinishedTransition.isNone) _pc2dRooms.OnFinishedTransition.RemoveListener (onFinishedTransitionCallBack);
 		if (!onExitedAllRooms.isNone) _pc2dRooms.OnExitedAllRooms.RemoveListener (onExitedAllRoomsCallBack);
@@ -69,6 +81,9 @@ public class PlayMakerPC2dRoomProxy : MonoBehaviour {
 			UnityEngine.Debug.Log("onStartedTransition room: "+roomIndex+" previous room:"+previousRoomIndex+" on "+this.gameObject.name);
 		}
 
+		SetEventProperties.properties ["roomIndex"] = roomIndex;
+		SetEventProperties.properties ["previousRoomIndex"] = previousRoomIndex;
+
 		onStartedTransition.SendEvent(null,eventTarget);
 	}
 
@@ -78,6 +93,9 @@ public class PlayMakerPC2dRoomProxy : MonoBehaviour {
 		{
 			UnityEngine.Debug.Log("onFinishedTransition room: "+roomIndex+" previous room:"+previousRoomIndex+" on "+this.gameObject.name);
 		}
+
+		SetEventProperties.properties ["roomIndex"] = roomIndex;
+		SetEventProperties.properties ["previousRoomIndex"] = previousRoomIndex;
 
 		onFinishedTransition.SendEvent(null,eventTarget);
 	}
